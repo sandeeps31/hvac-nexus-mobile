6cb50723c8b3612acebc441accdb6baab6c6401e
sandeeps31 <sandeeps31@hotmail.com>
2026-04-17 18:56:06 +1000

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Service Worker: offline caching + background sync
    ============================================================ */
 
-var CACHE_NAME = 'hvacnexus-mobile-v1';
+var CACHE_NAME = 'hvacnexus-mobile-v2';
 
 // Files to cache for offline use
 var PRECACHE = [
