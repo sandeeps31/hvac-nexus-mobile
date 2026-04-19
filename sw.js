b5835884f96868c581afcfc731a8b91a8cc6efd7
sandeeps31 <sandeeps31@hotmail.com>
2026-04-19 20:01:00 +1000

    dwg

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Service Worker: offline caching + background sync
    ============================================================ */
 
-var CACHE_NAME = 'hvacnexus-mobile-v25';
+var CACHE_NAME = 'hvacnexus-mobile-v26';
 
 // Files to cache for offline use
 var PRECACHE = [
